271404b53625470095e68013d494d55ed8448942
Charles Blake <charlechaud@gmail.com>
2022-08-01 16:10:59 -0400

    Use `grAlg` to analyze connected components of a pre-parsed thesaurus.

---
+++ util/compon.nim
@@ -0,0 +1,17 @@
+import std/[os, memfiles], thes, grAlg
+if paramCount() < 1:
+  quit "Usage:\n\tcompon BASE\nshow graph components", 1
+
+let th = thOpen("", getEnv("HOME", "/u/cb")/".config/thes/"/paramStr(1))
+
+iterator edges[I](th: Thes, u: I): I =
+  for v in th.synos(th.word(u.I)[0]): yield v.abs
+
+for scc in values(th.unDirCompons(th.maxWRef.int,words,edges)):
+  if scc.len < 100:
+    echo "Component:"
+    for e in scc: echo "  ", th.word(e.int32.abs)[0]
+  else:
+    echo "Component: ", scc.len, " words"
+
+echo th.unDirComponSizes(th.maxWRef.int, words, edges)
