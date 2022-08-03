44854aca3b859b111bfb4abc38c19e67dffc98ab
Charles Blake <charlechaud@gmail.com>
2022-08-03 14:53:20 -0400

    Add `wedges` iterators for proper weighted Dijkstra algo, update test cases, add 2nd hot cache Dijkstra test case and timeIt the build of the thesaurus in the from-scratch variant and add kinda neat fewest characters through the hops path search test.

--- util/synoPath.nim
+++ util/synoPath.nim
@@ -8,6 +8,12 @@ let th = thOpen("", getEnv("HOME", "/u/cb")/".config/thes/"/paramStr(1))
 iterator edges[I](th: Thes, u: I): I =
   for v in th.synos(th.word(u.I)[0]): yield v.abs
 
+iterator wedges1[I](th: Thes, u: I): (I, uint32) =
+  for v in th.synos(th.word(u.I)[0]): yield (v.abs, 1u32)
+
+iterator wedges[I](th: Thes, u: I): (I, uint32) =
+  for v in th.synos(th.word(u.I)[0]): yield (v.abs, th.word(v.abs)[0].size.uint32)
+
 var b = th.find(paramStr(2).toMemSlice).int32
 if b < 0: quit paramStr(2) & ": not found", 1
 b = th.tab[b].kwR.int32
@@ -17,8 +23,16 @@ if e < 0: quit paramStr(3) & ": not found", 1
 e = th.tab[e].kwR.int32
 
 timeIt(" Dijkstra Shortest Path"):
-  let pf = th.shortestPathPFS(uint32, th.maxWRef.int, b, e, words, edges)
+  let pf = th.shortestPathPFS(uint32, th.maxWRef.int, b, e, words, wedges1)
 for r in pf: echo "  ", th.word(r.abs)[0]
+
+timeIt(" Dijkstra Hot Cache"):
+  discard th.shortestPathPFS(uint32, th.maxWRef.int, b, e, words, wedges1)
+
 timeIt(" Breadth First Search"):
   let bf = th.shortestPathBFS(th.maxWRef.int, b, e, edges)
 for r in bf: echo "  ", th.word(r.abs)[0]
+
+timeIt(" Dijkstra Min Chars Path"):
+  let mc = th.shortestPathPFS(uint32, th.maxWRef.int, b, e, words, wedges)
+for r in mc: echo "  ", th.word(r.abs)[0]
--- util/synoPathFS.nim
+++ util/synoPathFS.nim
@@ -3,7 +3,7 @@ template timeIt(label, stmt) =
   let t0 = epochTime(); stmt; echo epochTime() - t0, " sec", label
 
 if paramCount() < 2: quit "Usage:\n\tpathSyno word1 word2 < wordsX.txt", 1
-let th = stdin.makeSymmetric adjRecip
+timeIt(" Thesaurus Build"): (let th = stdin.makeSymmetric adjRecip)
 
 iterator nodes(th: Thes): int32 =
   for i in 0i32 ..< toStr.len.int32: yield i
@@ -11,11 +11,26 @@ iterator nodes(th: Thes): int32 =
 iterator edges(th: Thes, u: int32): int32 =
   for v in th[u]: yield v
 
+iterator wedges1(th: Thes, u: int32): (int32, uint32) =
+  for v in th[u]: yield (v, 1u32)
+
+iterator wedges(th: Thes, u: int32): (int32, uint32) =
+  for v in th[u]: yield (v, toStr[u].len.uint32)
+
 let b = getId(paramStr(1))
 let e = getId(paramStr(2))
+
 timeIt(" Dijkstra Shortest Path"):
-  let pf = th.shortestPathPFS(uint32, toStr.len, b, e, nodes, edges)
+  let pf = th.shortestPathPFS(uint32, toStr.len, b, e, nodes, wedges1)
 for r in pf: echo "  ", toStr[r]
+
+timeIt(" Dijkstra Hot Cache"):
+  discard th.shortestPathPFS(uint32, toStr.len, b, e, nodes, wedges1)
+
 timeIt(" Breadth First Search"):
   let bf = th.shortestPathBFS(toStr.len, b, e, edges)
 for r in bf: echo "  ", toStr[r]
+
+timeIt(" Dijkstra Min Chars Path"):
+  let mc = th.shortestPathPFS(uint32, toStr.len, b, e, nodes, wedges)
+for r in mc: echo "  ", toStr[r]
