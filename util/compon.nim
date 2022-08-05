23125518517cc271b5972ddc762ac70a07a0b2e4
Charles Blake <charlechaud@gmail.com>
2022-08-05 11:05:18 -0400

    Maybe better ident names.

--- util/compon.nim
+++ util/compon.nim
@@ -7,11 +7,11 @@ let th = thOpen("", getEnv("HOME", "/u/cb")/".config/thes/"/paramStr(1))
 iterator edges[I](th: Thes, u: I): I =
   for v in th.synos(th.word(u.I)[0]): yield v.abs
 
-for scc in values(th.unDirCompons(th.maxWRef.int,words,edges)):
-  if scc.len < 100:
+for cc in values(th.unDirCompons(th.maxWRef.int,words,edges)):
+  if cc.len < 100:
     echo "Component:"
-    for e in scc: echo "  ", th.word(e.int32.abs)[0]
+    for elt in cc: echo "  ", th.word(elt.int32.abs)[0]
   else:
-    echo "Component: ", scc.len, " words"
+    echo "Component: ", cc.len, " words"
 
 echo th.unDirComponSizes(th.maxWRef.int, words, edges)
